3a184112ab700e7dd61ab05e010956d9d43826c1
Ponkore <ponkore@me.com>
2013-09-16 20:26:59 +0900

    version up template engine change (clabango -> selmer)

--- project.clj
+++ project.clj
@@ -2,14 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.5.0"]
+                 [lib-noir "0.6.9"]
                  [compojure "1.1.5"]
-                 [ring-server "0.2.7"]
-                 [clabango "0.5"]
-                 [com.taoensso/timbre "1.5.2"]
-                 [com.taoensso/tower "1.5.1"]
-                 [markdown-clj "0.9.19"]]
-  :plugins [[lein-ring "0.8.3"]]
+                 [ring-server "0.3.0"]
+                 [selmer "0.4.2"]
+                 [com.taoensso/timbre "2.6.1"]
+                 [com.postspectacular/rotor "0.1.0"]
+                 [com.taoensso/tower "1.7.1"]
+                 [markdown-clj "0.9.31"]]
+  :plugins [[lein-ring "0.8.7"]]
   :ring {:handler mapmap.handler/war-handler
          :init    mapmap.handler/init
          :destroy mapmap.handler/destroy}
@@ -17,6 +18,6 @@
   {:production {:ring {:open-browser? false
                        :stacktraces?  false
                        :auto-reload?  false}}
-   :dev {:dependencies [[ring-mock "0.1.3"]
-                        [ring/ring-devel "1.1.8"]]}}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.2.0"]]}}
   :min-lein-version "2.0.0")
