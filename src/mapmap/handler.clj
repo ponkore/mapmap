b0b1bde7bf88f5f2c802f258d65ac5a1b63cb669
Ponkore <ponkore@me.com>
2013-09-16 20:31:30 +0900

    update luminus default code.

--- src/mapmap/handler.clj
+++ src/mapmap/handler.clj
@@ -1,8 +1,10 @@
-(ns mapmap.handler
-  (:use mapmap.routes.home
-        compojure.core)
-  (:require [noir.util.middleware :as middleware]
-            [compojure.route :as route]))
+(ns mapmap.handler  
+  (:require [compojure.core :refer [defroutes]]            
+            [mapmap.routes.home :refer [home-routes]]
+            [noir.util.middleware :as middleware]
+            [compojure.route :as route]
+            [taoensso.timbre :as timbre]
+            [com.postspectacular.rotor :as rotor]))
 
 (defroutes app-routes
   (route/resources "/")
@@ -14,20 +16,33 @@
    an app server such as Tomcat
    put any initialization code here"
   []
-  (println "mapmap started successfully..."))
+  (timbre/set-config!
+    [:appenders :rotor]
+    {:min-level :info
+     :enabled? true
+     :async? false ; should be always false for rotor
+     :max-message-per-msecs nil
+     :fn rotor/append})
+  
+  (timbre/set-config!
+    [:shared-appender-config :rotor]
+    {:path "mapmap.log" :max-size (* 512 1024) :backlog 10})
+  
+  (timbre/info "mapmap started successfully"))
 
 (defn destroy
   "destroy will be called when your application
    shuts down, put any clean up code here"
   []
-  (println "shutting down..."))
+  (timbre/info "mapmap is shutting down..."))
 
-;;append your application routes to the all-routes vector
-(def all-routes [home-routes app-routes])
-
-(def app (-> all-routes
-             middleware/app-handler
-             ;;add your middlewares here
-             ))
+(def app (middleware/app-handler
+           ;;add your application routes here
+           [home-routes app-routes]
+           ;;add custom middleware here
+           :middleware []
+           ;;add access rules here
+           ;;each rule should be a vector
+           :access-rules []))
 
 (def war-handler (middleware/war-handler app))
--- src/mapmap/views/layout.clj
+++ src/mapmap/views/layout.clj
@@ -1,10 +1,17 @@
 (ns mapmap.views.layout
-  (:use noir.request)
-  (:require [clabango.parser :as parser]))
+  (:require [selmer.parser :as parser]
+            [ring.util.response :refer [response]])
+  (:import compojure.response.Renderable))
 
 (def template-path "mapmap/views/templates/")
 
+(deftype RenderableTemplate [template params]
+  Renderable
+  (render [this request]
+    (->> (assoc params :servlet-context (:context request))
+         (parser/render-file (str template-path template))
+         response)))
+
 (defn render [template & [params]]
-  (parser/render-file (str template-path template)
-                      (assoc params :context (:context *request*))))
+  (RenderableTemplate. template params))
 
