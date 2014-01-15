1f9266a6af45eeb2421e39f41d941b5cfa3e39d3
Ponkore <ponkore@gmail.com>
2014-01-15 22:31:56 +0900

    add header alias name and some data added.

--- resources/public/angular-js-study/sample-06.js
+++ resources/public/angular-js-study/sample-06.js
@@ -1,9 +1,10 @@
 angular.module('myapp', ['ui.bootstrap', 'ngGrid']);
 var GridDemoCtrl = function ($scope) {
-  $scope.myData = [{name: "aaa", age: 50},
-                   {name: "bbb", age: 43},
-                   {name: "ccc", age: 27},
-                   {name: "ddd", age: 29},
-                   {name: "eee", age: 34}];
-  $scope.myOptions = { data: 'myData' };
+  $scope.myData = [{name: "吹雪", age: 50},
+                   {name: "赤城", age: 43},
+                   {name: "金剛", age: 27},
+                   {name: "島風", age: 29},
+                   {name: "雷", age: 34}];
+  $scope.myOptions = { data: 'myData',
+                       columnDefs: [{field:'name', displayName:'名前'}, {field:'age', displayName:'年齢'}]};
 };
