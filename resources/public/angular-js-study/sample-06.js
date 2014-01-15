c4874cae8c3cc64c476ee56e51c15fbfdcc12b99
Ponkore <ponkore@gmail.com>
2014-01-15 23:58:43 +0900

    fix indent.

--- resources/public/angular-js-study/sample-06.js
+++ resources/public/angular-js-study/sample-06.js
@@ -5,10 +5,11 @@ var GridDemoCtrl = function ($scope) {
                    {name: "金剛", age: 27},
                    {name: "島風", age: 29},
                    {name: "雷", age: 34}];
-  $scope.myOptions = { data: 'myData',
-                       columnDefs: [
-                           {field:'name', displayName:'名前'},
-                           {field:'age', displayName:'年齢',
-                                cellTemplate: 'sample-06-cellTemplate.html'}]
-        };
+  $scope.myOptions = {
+      data: 'myData',
+      columnDefs: [
+        {field:'name', displayName:'名前'},
+        {field:'age', displayName:'年齢', cellTemplate: 'sample-06-cellTemplate.html'}
+      ]
+  };
 };
