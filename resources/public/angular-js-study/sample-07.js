0eec9eacd706ec833895be43657808026b697d94
Ponkore <ponkore@gmail.com>
2014-01-18 00:38:36 +0900

    retreive data from luminus .clj code

--- resources/public/angular-js-study/sample-07.js
+++ resources/public/angular-js-study/sample-07.js
@@ -20,17 +20,19 @@ app.controller('GridDemoCtrl', function($scope, $http) {
     };
     $scope.getPagedDataAsync = function(pageSize, page, searchText) {
         setTimeout(function() {
+       //     var jsonsrc = 'jsonFiles/largeLoad.json';
+            var jsonsrc = '/test';
             var data;
             if (searchText) {
                 var ft = searchText.toLowerCase();
-                $http.get('jsonFiles/largeLoad.json').success(function(largeLoad) {
+                $http.get(jsonsrc).success(function(largeLoad) {
                     data = largeLoad.filter(function(item) {
                         return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
                     });
                     $scope.setPagingData(data,page,pageSize);
                 });
             } else {
-                $http.get('jsonFiles/largeLoad.json').success(function(largeLoad) {
+                $http.get(jsonsrc).success(function(largeLoad) {
                     $scope.setPagingData(largeLoad,page,pageSize);
                 });
             }
--- src/mapmap/routes/home.clj
+++ src/mapmap/routes/home.clj
@@ -1,7 +1,8 @@
 (ns mapmap.routes.home
   (:use compojure.core)
   (:require [mapmap.views.layout :as layout]
-            [mapmap.util :as util]))
+            [mapmap.util :as util]
+            [noir.response :refer [json]]))
 
 (defn home-page []
   (layout/render
@@ -10,6 +11,18 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn- sample-records
+  []
+  [{:name "Moroni", :allowance 50, :paid true},
+   {:name "Tiancum", :allowance 53, :paid false},
+   {:name "Jacob", :allowance 27, :paid false},
+   {:name "Nephi", :allowance 29, :paid false},
+   {:name "Enos", :allowance 34, :paid false},
+   {:name "Ether", :allowance 42, :paid false},
+   {:name "Alma", :allowance 43, :paid true},
+   {:name "Jared", :allowance 21, :paid true}])
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/test" [] (json (sample-records))))
