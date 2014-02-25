8ac097490a90e371bf20eec537722db5764b9382
Ponkore <ponkore@gmail.com>
2014-02-25 23:13:39 +0900

    angularjs test code removed.

--- src/mapmap/routes/home.clj
+++ src/mapmap/routes/home.clj
@@ -11,18 +11,6 @@
 (defn about-page []
   (layout/render "about.html"))
 
-(defn- sample-records
-  []
-  [{:name "Moroni", :allowance 50, :paid true},
-   {:name "Tiancum", :allowance 53, :paid false},
-   {:name "Jacob", :allowance 27, :paid false},
-   {:name "Nephi", :allowance 29, :paid false},
-   {:name "Enos", :allowance 34, :paid false},
-   {:name "Ether", :allowance 42, :paid false},
-   {:name "Alma", :allowance 43, :paid true},
-   {:name "Jared", :allowance 21, :paid true}])
-
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page))
-  (GET "/test" [] (json (sample-records))))
+  (GET "/about" [] (about-page)))
