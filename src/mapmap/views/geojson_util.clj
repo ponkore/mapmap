c2fb28ea7c6940d50df650d45b411ecf039d7aec
Masao KATO <masao@ncap4147.local>
2014-03-05 20:27:59 +0900

    add views/geojson-views ns.

---
+++ src/mapmap/views/geojson_util.clj
@@ -0,0 +1,28 @@
+(ns mapmap.views.geojson-util
+  (:require [clojure.data.json :as json]
+            [mapmap.model.geojson :as model]))
+
+(defn- to-station-feature-map
+  ""
+  [info]
+  (assoc {}
+    :type "Feature"
+    :id (:id info)
+    :properties {:name (:station-name info)}
+    :geometry (:geometry info)))
+
+(defn- to-line-feature-map
+  ""
+  [info]
+  (assoc {}
+    :type "Feature"
+    :id (:id info)
+    :properties {:name (:line-name info)}
+    :geometry (:geometry info)))
+
+(defn make-feature-collection
+  ""
+  [coll]
+  (assoc {}
+    :type "FeatureCollection"
+    :features coll))
---
+++ test/mapmap/views/geojson_util_test.clj
@@ -0,0 +1,12 @@
+(ns mapmap.views.geojson-util-test
+  (:require [mapmap.views.geojson-util :refer :all]
+            [midje.sweet :refer :all]))
+
+(fact "to-station-feature-map"
+  )
+
+(fact "to-line-feature-map"
+  )
+
+(fact "make-feature-collection"
+  )
