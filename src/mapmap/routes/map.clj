4d3b2ffdbcef59925c8da79c9282631719bb1fa3
Masao KATO <masao@ncap4147.local>
2014-03-06 20:47:55 +0900

    refactoring

--- src/mapmap/routes/map.clj
+++ src/mapmap/routes/map.clj
@@ -5,32 +5,24 @@
             [mapmap.model.geojson :as model]
             [noir.response :refer [json]]))
 
-(defn- map-page
-  "returns JSON string"
-  [id]
-  (slurp (str "src/mapmap/model/json/" id)))
-
-(defn- stations-json
+(defn- find-stations
   ""
   [params]
   (let [query-params (:query-params params)
-        left (-> (get query-params "left") Double/valueOf)
-        top (-> (get query-params "top") Double/valueOf)
-        right (-> (get query-params "right") Double/valueOf)
-        bottom (-> (get query-params "bottom") Double/valueOf)
+        get-double-fn (fn [key param] (-> (get param key) Double/valueOf))
+        left   (get-double-fn "left" query-params)
+        top    (get-double-fn "top" query-params)
+        right  (get-double-fn "right" query-params)
+        bottom (get-double-fn "bottom" query-params)
         filter-fn (fn [info]
                     (when-let [[lon lat] (:geometry info)]
                       (and (<= left lon right) (<= bottom lat top))))]
     (->> (model/get-stations filter-fn)
          (map gj-util/to-station-feature-map)
-         (gj-util/make-feature-collection)
-         (json))))
-
-(defn- map-view
-  ""
-  []
-  (layout/render "ng-map.html" {}))
+         (gj-util/make-feature-collection))))
 
 (defroutes map-routes
-  (GET "/mapview" [] (map-view))
-  (GET "/map/stations" params (stations-json params)))
+  ;; main view page
+  (GET "/mapview" [] (layout/render "ng-map.html" {}))
+  ;; station list json generator
+  (GET "/map/stations" params (-> params find-stations json)))
